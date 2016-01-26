dce574c50a8e931eef49330bde3d0db82732ee12
thbe <project@bendler-net.de>
2016-01-26 17:47:26 +0100

    Fixed invalid variable name

--- manifests/config/director.pp
+++ manifests/config/director.pp
@@ -39,98 +39,98 @@ class bareos::config::director {
       mode    => '0644',
       content => template($bareos::params::config_director_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_catalog:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_catalog_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_client_director:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_client_director_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_console:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_console_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_director:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_director_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_fileset:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_fileset_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_job_backup_catalog:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_job_backup_catalog_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_job_director:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_job_director_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_job_restore:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_job_restore_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_jobdefs:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_jobdefs_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_messages:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_messages_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_pool:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_pool_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_schedule:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_schedule_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
 
     $bareos::params::config_confd_storage:
       ensure  => file,
       mode    => '0644',
       content => template($bareos::params::config_confd_storage_template),
       notify  => Service[$bareos::params::service_director],
-      require => Package[$bareos::params::package_director_common];
+      require => Package[$bareos::params::package_director];
   }
 
   # Create client configuration
